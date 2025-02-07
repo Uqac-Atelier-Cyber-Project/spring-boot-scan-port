98aea5952525ce8e3d7d68705f2b9a3de68a5349
Aubry TONNERRE <47123188+kiurow590@users.noreply.github.com>
2025-02-07 03:41:34 +0000

    init project file

---
+++ src/main/java/com/uqac/scan_port/ScanPortApplication.java
@@ -0,0 +1,13 @@
+package com.uqac.scan_port;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanPortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanPortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uqac/scan_port/ScanPortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uqac.scan_port;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanPortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
